e29525a23265f807b9c454f50db21cf4edd05d72
Pierre Congiu <pierre.congiu@nikolamotor.com>
2024-02-01 16:30:21 -0700

    add main to .c file

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,14 @@
 #include "HelloWorld.h"
+#include <stdio.h> 
 
 const char* greetings()
 {
 	return "Hello World";
+}
+   
+int main()
+{ 
+    // Displays the string inside quotations
+    printf("C Programming");
+    return 0;
 }
